f1988392135bcc3c1f951da7451f5339963c0a66
aelassas <akram.elassas@gmail.com>
2020-04-21 19:54:03 +0000

    Added multi language support to login page

---
+++ src/backend/Wexflow.Backend/js/language.js
@@ -0,0 +1,94 @@
+﻿function Language() {
+    "use strict";
+
+    let self = this;
+
+    let languages = {};
+    languages["en"] = {};
+    languages["fr"] = {};
+
+    // en
+    languages["en"]["help"] = "Help";
+    languages["en"]["about"] = "About";
+    languages["en"]["username"] = "Username";
+    languages["en"]["password"] = "Password";
+    languages["en"]["forgot-password"] = "Forgot Password?";
+    languages["en"]["login"] = "Sign in";
+    languages["en"]["valid-username"] = "Enter a valid username and password.";
+    languages["en"]["wrong-credentials"] = "Wrong credentials.";
+    languages["en"]["wrong-password"] = "The password is incorrect.";
+
+    // fr
+    languages["fr"]["help"] = "Aide";
+    languages["fr"]["about"] = "À propos";
+    languages["fr"]["username"] = "Utilisateur";
+    languages["fr"]["password"] = "Mot de passe";
+    languages["fr"]["forgot-password"] = "Mot de passe oublié ?";
+    languages["fr"]["login"] = "Se connecter";
+    languages["fr"]["valid-username"] = "Entrez un nom d'utilisateur et un mot de passe valides.";
+    languages["fr"]["wrong-credentials"] = "Paramètres de connexion incorrects.";
+    languages["fr"]["wrong-password"] = "Mot de passe incorrect.";
+
+    this.get = function (keyword) {
+        return languages[self.getLanguage()][keyword] || languages["en"][keyword];
+    }
+
+    this.setLanguage = function (code) {
+        set("language", code);
+    };
+
+    this.getLanguage = function () {
+        let code = get("language");
+        if (!code) {
+            return "en";
+        }
+        return code;
+    }
+
+    function set(key, value) {
+        if (isIE()) {
+            setCookie(key, value, 365);
+        } else {
+            window.localStorage.setItem(key, value);
+        }
+    }
+
+    function get(key) {
+        if (isIE()) {
+            return getCookie(key);
+        } else {
+            return window.localStorage.getItem(key);
+        }
+    }
+
+    function setCookie(cname, cvalue, exdays) {
+        let d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    function getCookie(cname) {
+        let name = cname + "=";
+        let ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    function isIE() {
+        let ua = navigator.userAgent;
+        /* MSIE used to detect old browsers and Trident used to newer ones*/
+        let is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+
+        return is_ie;
+    }
+
+}
--- src/backend/Wexflow.Backend/js/login.js
+++ src/backend/Wexflow.Backend/js/login.js
@@ -1,10 +1,41 @@
 function Login() {
 
-    var uri = Common.trimEnd(Settings.Uri, "/");
-    var loginBtn = document.getElementById("btn-login");
-    var usernameTxt = document.getElementById("txt-username");
-    var passwordTxt = document.getElementById("txt-password");
-    var auth = "";
+    // lang
+    let language = new Language();
+
+    function updateLanguage() {
+        let code = language.getLanguage();
+        if (code === "en") {
+            document.getElementById("lang").title = "French";
+            document.getElementById("lang-img").src = "images/fr.png";
+        } else if (code === "fr") {
+            document.getElementById("lang").title = "English";
+            document.getElementById("lang-img").src = "images/en.png";
+        }
+        document.getElementById("help").innerHTML = language.get("help");
+        document.getElementById("about").innerHTML = language.get("about");
+        document.getElementById("lbl-username").innerHTML = language.get("username");
+        document.getElementById("lbl-password").innerHTML = language.get("password");
+        document.getElementById("forgot-password").innerHTML = language.get("forgot-password");
+        document.getElementById("btn-login").value = language.get("login");
+    }
+    updateLanguage();
+
+    document.getElementById("lang").onclick = function () {
+        let code = language.getLanguage();
+        if (code === "en") {
+            language.setLanguage("fr");
+        } else if (code === "fr") {
+            language.setLanguage("en");
+        }
+        updateLanguage();
+    };
+
+    let uri = Common.trimEnd(Settings.Uri, "/");
+    let loginBtn = document.getElementById("btn-login");
+    let usernameTxt = document.getElementById("txt-username");
+    let passwordTxt = document.getElementById("txt-password");
+    let auth = "";
 
     loginBtn.onclick = function () {
         login();
@@ -20,23 +51,23 @@ function Login() {
 
     function login() {
 
-        var username = usernameTxt.value;
-        var password = passwordTxt.value;
-        var passwordHash = MD5(password);
+        let username = usernameTxt.value;
+        let password = passwordTxt.value;
+        let passwordHash = MD5(password);
         auth = "Basic " + btoa(username + ":" + passwordHash);
 
         if (username === "" || password === "") {
-            Common.toastInfo("Enter a valid username and password.");
+            Common.toastInfo(language.get("valid-username"));
         } else {
             Common.get(uri + "/user?username=" + encodeURIComponent(username), function (user) {
                 if (typeof user === "undefined" || user === null) {
-                    Common.toastError("Wrong credentials.");
+                    Common.toastError(language.get("wrong-credentials"));
                 } else {
                     if (passwordHash === user.Password) {
                         authorize(username, passwordHash, user.UserProfile);
                         window.location.replace("dashboard.html");
                     } else {
-                        Common.toastError("The password is incorrect.");
+                        Common.toastError(language.get("wrong-password"));
                     }
 
                 }
