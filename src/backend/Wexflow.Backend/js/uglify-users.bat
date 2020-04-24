0812ff85d49eb894a76a836d9949a0e18e5fed01
aelassas <akram.elassas@gmail.com>
2020-04-24 11:14:40 +0000

    Updated users page

--- src/backend/Wexflow.Backend/js/uglify-users.bat
+++ src/backend/Wexflow.Backend/js/uglify-users.bat
@@ -1,1 +1,1 @@
-uglifyjs jquery.min.js jquery.toast.min.js common.js md5.js authenticate.js users.js -o users.min.js -c -m
+uglifyjs jquery.min.js jquery.toast.min.js bootstrap.min.js common.js md5.js authenticate.js language.core.js users.js -o users.min.js -c -m
