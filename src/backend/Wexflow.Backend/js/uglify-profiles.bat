6290e89ddd2377271582f6a5d4501cf4a7008b34
aelassas <akram.elassas@gmail.com>
2020-04-24 11:16:37 +0000

    Updated profiles page

--- src/backend/Wexflow.Backend/js/uglify-profiles.bat
+++ src/backend/Wexflow.Backend/js/uglify-profiles.bat
@@ -1,1 +1,1 @@
-uglifyjs jquery.min.js jquery.toast.min.js common.js md5.js authenticate.js profiles.js -o profiles.min.js -c -m
+uglifyjs jquery.min.js jquery.toast.min.js bootstrap.min.js common.js md5.js authenticate.js language.core.js profiles.js -o profiles.min.js -c -m
