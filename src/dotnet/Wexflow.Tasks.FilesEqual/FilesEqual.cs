f9b7e0cec3550a33d87cebb855e92e90e0320dfa
aelassas <akram.elassas@gmail.com>
2020-04-26 16:58:26 +0000

    Added samba support to FilesEqual (#219)

--- src/dotnet/Wexflow.Tasks.FilesEqual/FilesEqual.cs
+++ src/dotnet/Wexflow.Tasks.FilesEqual/FilesEqual.cs
@@ -10,58 +10,40 @@ namespace Wexflow.Tasks.FilesEqual
     {
         public string File1 { get; set; }
         public string File2 { get; set; }
+        public string SmbComputerName { get; private set; }
+        public string SmbDomain { get; private set; }
+        public string SmbUsername { get; private set; }
+        public string SmbPassword { get; private set; }
 
-        public FilesEqual(XElement xe, Workflow wf)
-            : base(xe, wf)
+        public FilesEqual(XElement xe, Workflow wf) : base(xe, wf)
         {
             File1 = GetSetting("file1");
             File2 = GetSetting("file2");
+            SmbComputerName = GetSetting("smbComputerName");
+            SmbDomain = GetSetting("smbDomain");
+            SmbUsername = GetSetting("smbUsername");
+            SmbPassword = GetSetting("smbPassword");
         }
 
         public override TaskStatus Run()
         {
             Info("Checking...");
 
-            bool success = true;
+            var success = true;
 
             try
             {
-                if (!File.Exists(File1))
+                if (!string.IsNullOrEmpty(SmbComputerName) && !string.IsNullOrEmpty(SmbUsername) && !string.IsNullOrEmpty(SmbPassword))
                 {
-                    Logger.ErrorFormat("The file {0} does not exist.", File1);
-                    return new TaskStatus(Status.Error, false);
+                    using (NetworkShareAccesser.Access(SmbComputerName, SmbDomain, SmbUsername, SmbPassword))
+                    {
+                        success = CheckFiles();
+                    }
                 }
-
-                if (!File.Exists(File2))
+                else
                 {
-                    Logger.ErrorFormat("The file {0} does not exist.", File2);
-                    return new TaskStatus(Status.Error, false);
+                    success = CheckFiles();
                 }
-
-                var xmlPath = Path.Combine(Workflow.WorkflowTempFolder,
-                       string.Format("FilesEqual_{0:yyyy-MM-dd-HH-mm-ss-fff}.xml", DateTime.Now));
-                var xdoc = new XDocument(new XElement("Root"));
-                var xFiles = new XElement("Files");
-
-                xFiles.Add(new XElement("File",
-                    new XAttribute("path", File1),
-                    new XAttribute("name", Path.GetFileName(File1))));
-
-                xFiles.Add(new XElement("File",
-                    new XAttribute("path", File2),
-                    new XAttribute("name", Path.GetFileName(File2))));
-                
-                if (xdoc.Root != null)
-                {
-                    xdoc.Root.Add(xFiles);
-
-                    var res = FileEquals(File1, File2);
-                    xdoc.Root.Add(new XElement("Result", res.ToString().ToLower()));
-                }
-
-                xdoc.Save(xmlPath);
-                Files.Add(new FileInf(xmlPath, Id));
-                InfoFormat("The result has been written in: {0}", xmlPath);
             }
             catch (ThreadAbortException)
             {
@@ -81,7 +63,50 @@ namespace Wexflow.Tasks.FilesEqual
             }
 
             Info("Task finished.");
-            return new TaskStatus(status, false);
+            return new TaskStatus(status);
+        }
+
+        private bool CheckFiles()
+        {
+
+            if (!File.Exists(File1))
+            {
+                Logger.ErrorFormat("The file {0} does not exist.", File1);
+                return false;
+            }
+
+            if (!File.Exists(File2))
+            {
+                Logger.ErrorFormat("The file {0} does not exist.", File2);
+                return false;
+            }
+
+            var xmlPath = Path.Combine(Workflow.WorkflowTempFolder,
+                   string.Format("FilesEqual_{0:yyyy-MM-dd-HH-mm-ss-fff}.xml", DateTime.Now));
+            var xdoc = new XDocument(new XElement("Root"));
+            var xFiles = new XElement("Files");
+
+            xFiles.Add(new XElement("File",
+                new XAttribute("path", File1),
+                new XAttribute("name", Path.GetFileName(File1))));
+
+            xFiles.Add(new XElement("File",
+                new XAttribute("path", File2),
+                new XAttribute("name", Path.GetFileName(File2))));
+
+            if (xdoc.Root != null)
+            {
+                xdoc.Root.Add(xFiles);
+
+                var res = FileEquals(File1, File2);
+                xdoc.Root.Add(new XElement("Result", res.ToString().ToLower()));
+            }
+
+            xdoc.Save(xmlPath);
+            Files.Add(new FileInf(xmlPath, Id));
+            InfoFormat("The result has been written in: {0}", xmlPath);
+
+            return true;
         }
 
         private bool FileEquals(string path1, string path2)
