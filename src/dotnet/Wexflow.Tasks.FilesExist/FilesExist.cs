884be6fc3063b2e28f936ce1b110a22eae15427d
aelassas <akram.elassas@gmail.com>
2020-04-26 11:57:48 +0000

    Added samba support to FilesExist (#219)

--- src/dotnet/Wexflow.Tasks.FilesExist/FilesExist.cs
+++ src/dotnet/Wexflow.Tasks.FilesExist/FilesExist.cs
@@ -6,16 +6,23 @@ using System.IO;
 
 namespace Wexflow.Tasks.FilesExist
 {
-    public class FilesExist:Task
+    public class FilesExist : Task
     {
         public string[] FFiles { get; private set; }
         public string[] Folders { get; private set; }
+        public string SmbComputerName { get; private set; }
+        public string SmbDomain { get; private set; }
+        public string SmbUsername { get; private set; }
+        public string SmbPassword { get; private set; }
 
-        public FilesExist(XElement xe, Workflow wf)
-            : base(xe, wf)
+        public FilesExist(XElement xe, Workflow wf) : base(xe, wf)
         {
             FFiles = GetSettings("file");
             Folders = GetSettings("folder");
+            SmbComputerName = GetSetting("smbComputerName");
+            SmbDomain = GetSetting("smbDomain");
+            SmbUsername = GetSetting("smbUsername");
+            SmbPassword = GetSetting("smbPassword");
         }
 
         public override TaskStatus Run()
@@ -26,37 +33,17 @@ namespace Wexflow.Tasks.FilesExist
 
             try
             {
-                var xmlPath = Path.Combine(Workflow.WorkflowTempFolder,
-                       string.Format("FilesExist_{0:yyyy-MM-dd-HH-mm-ss-fff}.xml", DateTime.Now));
-                var xdoc = new XDocument(new XElement("Root"));
-                var xFiles = new XElement("Files");
-                var xFolders = new XElement("Folders");
-
-                foreach (var file in FFiles)
+                if (!string.IsNullOrEmpty(SmbComputerName) && !string.IsNullOrEmpty(SmbUsername) && !string.IsNullOrEmpty(SmbPassword))
                 {
-                    xFiles.Add(new XElement("File",
-                        new XAttribute("path", file),
-                        new XAttribute("name", Path.GetFileName(file)),
-                        new XAttribute("exists", File.Exists(file))));
+                    using (NetworkShareAccesser.Access(SmbComputerName, SmbDomain, SmbUsername, SmbPassword))
+                    {
+                        CheckFiles();
+                    }
                 }
-
-                foreach (var folder in Folders)
+                else
                 {
-                    xFolders.Add(new XElement("Folder",
-                        new XAttribute("path", folder),
-                        new XAttribute("name", Path.GetFileName(folder)),
-                        new XAttribute("exists", Directory.Exists(folder))));
+                    CheckFiles();
                 }
-
-                if (xdoc.Root != null)
-                {
-                    xdoc.Root.Add(xFiles);
-                    xdoc.Root.Add(xFolders);
-                }
-
-                xdoc.Save(xmlPath);
-                Files.Add(new FileInf(xmlPath, Id));
-                InfoFormat("The result has been written in: {0}", xmlPath);
             }
             catch (ThreadAbortException)
             {
@@ -77,6 +64,41 @@ namespace Wexflow.Tasks.FilesExist
 
             Info("Task finished.");
             return new TaskStatus(status, false);
+        }
+
+        private void CheckFiles()
+        {
+            var xmlPath = Path.Combine(Workflow.WorkflowTempFolder,
+                       string.Format("FilesExist_{0:yyyy-MM-dd-HH-mm-ss-fff}.xml", DateTime.Now));
+            var xdoc = new XDocument(new XElement("Root"));
+            var xFiles = new XElement("Files");
+            var xFolders = new XElement("Folders");
+
+            foreach (var file in FFiles)
+            {
+                xFiles.Add(new XElement("File",
+                    new XAttribute("path", file),
+                    new XAttribute("name", Path.GetFileName(file)),
+                    new XAttribute("exists", File.Exists(file))));
+            }
+
+            foreach (var folder in Folders)
+            {
+                xFolders.Add(new XElement("Folder",
+                    new XAttribute("path", folder),
+                    new XAttribute("name", Path.GetFileName(folder)),
+                    new XAttribute("exists", Directory.Exists(folder))));
+            }
+
+            if (xdoc.Root != null)
+            {
+                xdoc.Root.Add(xFiles);
+                xdoc.Root.Add(xFolders);
+            }
+
+            xdoc.Save(xmlPath);
+            Files.Add(new FileInf(xmlPath, Id));
+            InfoFormat("The result has been written in: {0}", xmlPath);
         }
     }
 }
