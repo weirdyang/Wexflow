91ac0eaa350f45d1cbf49cf9b5c1f939b0e1304b
aelassas <akram.elassas@gmail.com>
2020-04-27 09:58:45 +0000

    Added samba support to Touch (#219)

--- src/dotnet/Wexflow.Tasks.Touch/Touch.cs
+++ src/dotnet/Wexflow.Tasks.Touch/Touch.cs
@@ -9,19 +9,69 @@ namespace Wexflow.Tasks.Touch
     public class Touch : Task
     {
         public string[] Tfiles { get; private set; }
+        public string SmbComputerName { get; private set; }
+        public string SmbDomain { get; private set; }
+        public string SmbUsername { get; private set; }
+        public string SmbPassword { get; private set; }
 
         public Touch(XElement xe, Workflow wf) : base(xe, wf)
         {
             Tfiles = GetSettings("file");
+            SmbComputerName = GetSetting("smbComputerName");
+            SmbDomain = GetSetting("smbDomain");
+            SmbUsername = GetSetting("smbUsername");
+            SmbPassword = GetSetting("smbPassword");
         }
 
         public override TaskStatus Run()
         {
             Info("Touching files...");
 
-            bool success = true;
-            bool atLeastOneSucceed = false;
+            var success = true;
+            var atLeastOneSucceed = false;
 
+            try
+            {
+                if (!string.IsNullOrEmpty(SmbComputerName) && !string.IsNullOrEmpty(SmbUsername) && !string.IsNullOrEmpty(SmbPassword))
+                {
+                    using (NetworkShareAccesser.Access(SmbComputerName, SmbDomain, SmbUsername, SmbPassword))
+                    {
+                        success = TouchFiles(ref atLeastOneSucceed);
+                    }
+                }
+                else
+                {
+                    success = TouchFiles(ref atLeastOneSucceed);
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                ErrorFormat("An error occured while touching files.", e);
+                success = false;
+            }
+
+            var status = Status.Success;
+
+            if (!success && atLeastOneSucceed)
+            {
+                status = Status.Warning;
+            }
+            else if (!success)
+            {
+                status = Status.Error;
+            }
+
+            Info("Task finished.");
+            return new TaskStatus(status, false);
+        }
+
+        private bool TouchFiles(ref bool atLeastOneSucceed)
+        {
+            var success = true;
             foreach (string file in Tfiles)
             {
                 try
@@ -42,20 +92,7 @@ namespace Wexflow.Tasks.Touch
                     success = false;
                 }
             }
-
-            var status = Status.Success;
-
-            if (!success && atLeastOneSucceed)
-            {
-                status = Status.Warning;
-            }
-            else if (!success)
-            {
-                status = Status.Error;
-            }
-
-            Info("Task finished.");
-            return new TaskStatus(status, false);
+            return success;
         }
 
         private void TouchFile(string file)
