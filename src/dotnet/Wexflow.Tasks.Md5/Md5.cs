426349c12fdc704836e9f46bbea70b6bfde4a834
aelassas <akram.elassas@gmail.com>
2020-04-27 16:07:57 +0000

    Added samba support to Md5 (#219)

--- src/dotnet/Wexflow.Tasks.Md5/Md5.cs
+++ src/dotnet/Wexflow.Tasks.Md5/Md5.cs
@@ -8,20 +8,70 @@ using System.Threading;
 
 namespace Wexflow.Tasks.Md5
 {
-    public class Md5:Task
+    public class Md5 : Task
     {
-        public Md5(XElement xe, Workflow wf)
-            : base(xe, wf)
+        public string SmbComputerName { get; private set; }
+        public string SmbDomain { get; private set; }
+        public string SmbUsername { get; private set; }
+        public string SmbPassword { get; private set; }
+
+        public Md5(XElement xe, Workflow wf) : base(xe, wf)
         {
+            SmbComputerName = GetSetting("smbComputerName");
+            SmbDomain = GetSetting("smbDomain");
+            SmbUsername = GetSetting("smbUsername");
+            SmbPassword = GetSetting("smbPassword");
         }
 
         public override TaskStatus Run()
         {
             Info("Generating MD5 sums...");
 
-            bool success = true;
-            bool atLeastOneSucceed = false;
+            var success = true;
+            var atLeastOneSuccess = false;
 
+            try
+            {
+                if (!string.IsNullOrEmpty(SmbComputerName) && !string.IsNullOrEmpty(SmbUsername) && !string.IsNullOrEmpty(SmbPassword))
+                {
+                    using (NetworkShareAccesser.Access(SmbComputerName, SmbDomain, SmbUsername, SmbPassword))
+                    {
+                        success = GenerateMd5(ref atLeastOneSuccess);
+                    }
+                }
+                else
+                {
+                    success = GenerateMd5(ref atLeastOneSuccess);
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                ErrorFormat("An error occured while calculating MD5 checksums.", e);
+                success = false;
+            }
+
+            var status = Status.Success;
+
+            if (!success && atLeastOneSuccess)
+            {
+                status = Status.Warning;
+            }
+            else if (!success)
+            {
+                status = Status.Error;
+            }
+
+            Info("Task finished.");
+            return new TaskStatus(status, false);
+        }
+
+        private bool GenerateMd5(ref bool atLeastOneSuccess)
+        {
+            var success = true;
             var files = SelectFiles();
 
             if (files.Length > 0)
@@ -43,8 +93,8 @@ namespace Wexflow.Tasks.Md5
                                 new XAttribute("md5", md5)));
                         }
                         InfoFormat("Md5 of the file {0} is {1}", file.Path, md5);
-                        
-                        if (!atLeastOneSucceed) atLeastOneSucceed = true;
+
+                        if (!atLeastOneSuccess) atLeastOneSuccess = true;
                     }
                     catch (ThreadAbortException)
                     {
@@ -59,20 +109,7 @@ namespace Wexflow.Tasks.Md5
                 xdoc.Save(md5Path);
                 Files.Add(new FileInf(md5Path, Id));
             }
-
-            var status = Status.Success;
-
-            if (!success && atLeastOneSucceed)
-            {
-                status = Status.Warning;
-            }
-            else if (!success)
-            {
-                status = Status.Error;
-            }
-
-            Info("Task finished.");
-            return new TaskStatus(status, false);
+            return success;
         }
 
         private string GetMd5(string filePath)
