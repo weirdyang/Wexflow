855dc06c31416f2fc3b6ac271c314922d5471370
aelassas <akram.elassas@gmail.com>
2020-04-27 19:12:49 +0000

    Added samba support to YamlToJson (#219)

--- src/dotnet/Wexflow.Tasks.YamlToJson/YamlToJson.cs
+++ src/dotnet/Wexflow.Tasks.YamlToJson/YamlToJson.cs
@@ -8,19 +8,70 @@ using YamlDotNet.Serialization;
 
 namespace Wexflow.Tasks.YamlToJson
 {
-    public class YamlToJson:Task
+    public class YamlToJson : Task
     {
+        public string SmbComputerName { get; private set; }
+        public string SmbDomain { get; private set; }
+        public string SmbUsername { get; private set; }
+        public string SmbPassword { get; private set; }
+
         public YamlToJson(XElement xe, Workflow wf) : base(xe, wf)
         {
+            SmbComputerName = GetSetting("smbComputerName");
+            SmbDomain = GetSetting("smbDomain");
+            SmbUsername = GetSetting("smbUsername");
+            SmbPassword = GetSetting("smbPassword");
         }
 
         public override TaskStatus Run()
         {
             Info("Converting YAML files to JSON files...");
 
-            var status = Status.Success;
             var success = true;
             var atLeastOneSuccess = false;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(SmbComputerName) && !string.IsNullOrEmpty(SmbUsername) && !string.IsNullOrEmpty(SmbPassword))
+                {
+                    using (NetworkShareAccesser.Access(SmbComputerName, SmbDomain, SmbUsername, SmbPassword))
+                    {
+                        success = ConvertFiles(ref atLeastOneSuccess);
+                    }
+                }
+                else
+                {
+                    success = ConvertFiles(ref atLeastOneSuccess);
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                ErrorFormat("An error occured while copying files.", e);
+                success = false;
+            }
+
+            var status = Status.Success;
+
+            if (!success && atLeastOneSuccess)
+            {
+                status = Status.Warning;
+            }
+            else if (!success)
+            {
+                status = Status.Error;
+            }
+
+            Info("Task finished.");
+            return new TaskStatus(status);
+        }
+
+        private bool ConvertFiles(ref bool atLeastOneSuccess)
+        {
+            var success = false;
             var yamlFiles = SelectFiles();
 
             foreach (var yamlFile in yamlFiles)
@@ -53,18 +104,7 @@ namespace Wexflow.Tasks.YamlToJson
                     success = false;
                 }
             }
-
-            if (!success && atLeastOneSuccess)
-            {
-                status = Status.Warning;
-            }
-            else if (!success)
-            {
-                status = Status.Error;
-            }
-
-            Info("Task finished.");
-            return new TaskStatus(status);
+            return success;
         }
     }
 }
