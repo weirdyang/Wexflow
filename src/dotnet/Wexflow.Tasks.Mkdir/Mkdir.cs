f568e3b324c2acbe95f971dd9fc097ccb6f66908
aelassas <akram.elassas@gmail.com>
2020-04-27 10:06:19 +0000

    Added samba support to Mkdir (#219)

--- src/dotnet/Wexflow.Tasks.Mkdir/Mkdir.cs
+++ src/dotnet/Wexflow.Tasks.Mkdir/Mkdir.cs
@@ -6,41 +6,52 @@ using System.Threading;
 
 namespace Wexflow.Tasks.Mkdir
 {
-    public class Mkdir:Task
+    public class Mkdir : Task
     {
         public string[] Folders { get; private set; }
+        public string SmbComputerName { get; private set; }
+        public string SmbDomain { get; private set; }
+        public string SmbUsername { get; private set; }
+        public string SmbPassword { get; private set; }
 
-        public Mkdir(XElement xe, Workflow wf)
-            : base(xe, wf)
+        public Mkdir(XElement xe, Workflow wf) : base(xe, wf)
         {
             Folders = GetSettings("folder");
+            SmbComputerName = GetSetting("smbComputerName");
+            SmbDomain = GetSetting("smbDomain");
+            SmbUsername = GetSetting("smbUsername");
+            SmbPassword = GetSetting("smbPassword");
         }
 
         public override TaskStatus Run()
         {
             Info("Creating folders...");
-            
-            bool success = true;
-            bool atLeastOneSucceed = false;
 
-            foreach (string folder in Folders)
+            var success = true;
+            var atLeastOneSucceed = false;
+
+            try
             {
-                try
+                if (!string.IsNullOrEmpty(SmbComputerName) && !string.IsNullOrEmpty(SmbUsername) && !string.IsNullOrEmpty(SmbPassword))
                 {
-                    if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
-                    InfoFormat("Folder {0} created.", folder);
-                    
-                    if (!atLeastOneSucceed) atLeastOneSucceed = true;
+                    using (NetworkShareAccesser.Access(SmbComputerName, SmbDomain, SmbUsername, SmbPassword))
+                    {
+                        success = CreateFolders(ref atLeastOneSucceed);
+                    }
                 }
-                catch (ThreadAbortException)
+                else
                 {
-                    throw;
+                    success = CreateFolders(ref atLeastOneSucceed);
                 }
-                catch (Exception e)
-                {
-                    ErrorFormat("An error occured while creating the folder {0}", e, folder);
-                    success = false;
-                }
+            }
+            catch (ThreadAbortException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                ErrorFormat("An error occured while creating folders.", e);
+                success = false;
             }
 
             var status = Status.Success;
@@ -56,6 +67,32 @@ namespace Wexflow.Tasks.Mkdir
 
             Info("Task finished.");
             return new TaskStatus(status, false);
+        }
+
+        private bool CreateFolders(ref bool atLeastOneSucceed)
+        {
+            var success = true;
+            foreach (string folder in Folders)
+            {
+                try
+                {
+                    if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
+                    InfoFormat("Folder {0} created.", folder);
+
+                    if (!atLeastOneSucceed) atLeastOneSucceed = true;
+                }
+                catch (ThreadAbortException)
+                {
+                    throw;
+                }
+                catch (Exception e)
+                {
+                    ErrorFormat("An error occured while creating the folder {0}", e, folder);
+                    success = false;
+                }
+            }
+            return success;
+
         }
     }
 }
