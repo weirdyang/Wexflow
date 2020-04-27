619547e5dc48992ed1b96932279b5b4fb4650f58
aelassas <akram.elassas@gmail.com>
2020-04-27 11:40:32 +0000

    Added samba support to FileMatch (#219)

--- src/dotnet/Wexflow.Tasks.FileMatch/FileMatch.cs
+++ src/dotnet/Wexflow.Tasks.FileMatch/FileMatch.cs
@@ -12,19 +12,65 @@ namespace Wexflow.Tasks.FileMatch
         public string Dir { get; private set; }
         public string Pattern { get; private set; }
         public bool Recursive { get; private set; }
+        public string SmbComputerName { get; private set; }
+        public string SmbDomain { get; private set; }
+        public string SmbUsername { get; private set; }
+        public string SmbPassword { get; private set; }
 
         public FileMatch(XElement xe, Workflow wf) : base(xe, wf)
         {
             Dir = GetSetting("dir");
             Pattern = GetSetting("pattern");
             Recursive = bool.Parse(GetSetting("recursive", "false"));
+            SmbComputerName = GetSetting("smbComputerName");
+            SmbDomain = GetSetting("smbDomain");
+            SmbUsername = GetSetting("smbUsername");
+            SmbPassword = GetSetting("smbPassword");
         }
 
         public override TaskStatus Run()
         {
             Info("Checking file...");
 
-            bool success = false;
+            var success = false;
+            TaskStatus status = null;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(SmbComputerName) && !string.IsNullOrEmpty(SmbUsername) && !string.IsNullOrEmpty(SmbPassword))
+                {
+                    using (NetworkShareAccesser.Access(SmbComputerName, SmbDomain, SmbUsername, SmbPassword))
+                    {
+                        success = CheckFile(ref status);
+                    }
+                }
+                else
+                {
+                    success = CheckFile(ref status);
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                ErrorFormat("An error occured while copying files.", e);
+                success = false;
+            }
+
+            Info("Task finished");
+
+            if (status != null)
+            {
+                return status;
+            }
+            return new TaskStatus(Status.Success, success);
+        }
+
+        private bool CheckFile(ref TaskStatus status)
+        {
+            var success = false;
             string fileFound = string.Empty;
 
             try
@@ -50,7 +96,6 @@ namespace Wexflow.Tasks.FileMatch
                     }
                 }
 
-
                 if (success)
                 {
                     InfoFormat("A file matching the pattern {0} was found in the directory {1} -> {2}.", Pattern, Dir, fileFound);
@@ -67,12 +112,9 @@ namespace Wexflow.Tasks.FileMatch
             catch (Exception e)
             {
                 ErrorFormat("An error occured while checking directory {0}. Error: {1}", Dir, e.Message);
-                return new TaskStatus(Status.Error, false);
+                status = new TaskStatus(Status.Error, false);
             }
-
-            Info("Task finished");
-
-            return new TaskStatus(Status.Success, success);
+            return success;
         }
     }
 }
