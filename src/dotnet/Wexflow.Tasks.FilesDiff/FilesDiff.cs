b3c2cab07413ab6f94d01e9ffa3385ca2b09fbc6
aelassas <akram.elassas@gmail.com>
2020-04-27 09:13:07 +0000

    Added samba support to FilesDiff (#219)

--- src/dotnet/Wexflow.Tasks.FilesDiff/FilesDiff.cs
+++ src/dotnet/Wexflow.Tasks.FilesDiff/FilesDiff.cs
@@ -13,11 +13,19 @@ namespace Wexflow.Tasks.FilesDiff
     {
         public string OldFile { get; set; }
         public string NewFile { get; set; }
+        public string SmbComputerName { get; private set; }
+        public string SmbDomain { get; private set; }
+        public string SmbUsername { get; private set; }
+        public string SmbPassword { get; private set; }
 
         public FilesDiff(XElement xe, Workflow wf) : base(xe, wf)
         {
             OldFile = GetSetting("oldFile");
             NewFile = GetSetting("newFile");
+            SmbComputerName = GetSetting("smbComputerName");
+            SmbDomain = GetSetting("smbDomain");
+            SmbUsername = GetSetting("smbUsername");
+            SmbPassword = GetSetting("smbPassword");
         }
 
         public override TaskStatus Run()
@@ -27,41 +35,17 @@ namespace Wexflow.Tasks.FilesDiff
             TaskStatus ts;
             try
             {
-                string oldText = File.ReadAllText(OldFile);
-                string newText = File.ReadAllText(NewFile);
-
-                var diffBuilder = new InlineDiffBuilder(new Differ());
-                var diff = diffBuilder.BuildDiffModel(oldText, newText);
-
-                var resultPath = Path.Combine(Workflow.WorkflowTempFolder,
-                       string.Format("FilesDiff_{0:yyyy-MM-dd-HH-mm-ss-fff}.diff", DateTime.Now));
-
-                using (StreamWriter sw = new StreamWriter(resultPath))
+                if (!string.IsNullOrEmpty(SmbComputerName) && !string.IsNullOrEmpty(SmbUsername) && !string.IsNullOrEmpty(SmbPassword))
                 {
-                    foreach (var line in diff.Lines)
+                    using (NetworkShareAccesser.Access(SmbComputerName, SmbDomain, SmbUsername, SmbPassword))
                     {
-                        switch (line.Type)
-                        {
-                            case ChangeType.Inserted:
-                                sw.Write("+ ");
-                                break;
-                            case ChangeType.Deleted:
-                                sw.Write("- ");
-                                break;
-                            case ChangeType.Modified:
-                                sw.Write("~ ");
-                                break;
-                            default:
-                                sw.Write("  ");
-                                break;
-                        }
-
-                        sw.WriteLine(line.Text);
+                        CheckFiles();
                     }
                 }
-
-                Files.Add(new FileInf(resultPath, Id));
-                InfoFormat("The diff of the old file '{0}' and the new file '{1}' was calculated with success.", Path.GetFileName(OldFile), Path.GetFileName(NewFile));
+                else
+                {
+                    CheckFiles();
+                }
                 ts = new TaskStatus(Status.Success);
             }
             catch (ThreadAbortException)
@@ -77,7 +61,47 @@ namespace Wexflow.Tasks.FilesDiff
 
             Info("Task finished.");
             return ts;
-            
+
+        }
+
+        private void CheckFiles()
+        {
+            string oldText = File.ReadAllText(OldFile);
+            string newText = File.ReadAllText(NewFile);
+
+            var diffBuilder = new InlineDiffBuilder(new Differ());
+            var diff = diffBuilder.BuildDiffModel(oldText, newText);
+
+            var resultPath = Path.Combine(Workflow.WorkflowTempFolder,
+                   string.Format("FilesDiff_{0:yyyy-MM-dd-HH-mm-ss-fff}.diff", DateTime.Now));
+
+            using (StreamWriter sw = new StreamWriter(resultPath))
+            {
+                foreach (var line in diff.Lines)
+                {
+                    switch (line.Type)
+                    {
+                        case ChangeType.Inserted:
+                            sw.Write("+ ");
+                            break;
+                        case ChangeType.Deleted:
+                            sw.Write("- ");
+                            break;
+                        case ChangeType.Modified:
+                            sw.Write("~ ");
+                            break;
+                        default:
+                            sw.Write("  ");
+                            break;
+                    }
+
+                    sw.WriteLine(line.Text);
+                }
+            }
+
+            Files.Add(new FileInf(resultPath, Id));
+            InfoFormat("The diff of the old file '{0}' and the new file '{1}' was calculated with success.", Path.GetFileName(OldFile), Path.GetFileName(NewFile));
+
         }
     }
 }
