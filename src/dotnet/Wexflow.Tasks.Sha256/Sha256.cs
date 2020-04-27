7cbfd6f8d162584f79478c186113befe15763a4e
aelassas <akram.elassas@gmail.com>
2020-04-27 16:20:38 +0000

    Added samba support to Sha256 (#219)

--- src/dotnet/Wexflow.Tasks.Sha256/Sha256.cs
+++ src/dotnet/Wexflow.Tasks.Sha256/Sha256.cs
@@ -10,18 +10,68 @@ namespace Wexflow.Tasks.Sha256
 {
     public class Sha256 : Task
     {
-        public Sha256(XElement xe, Workflow wf)
-            : base(xe, wf)
+        public string SmbComputerName { get; private set; }
+        public string SmbDomain { get; private set; }
+        public string SmbUsername { get; private set; }
+        public string SmbPassword { get; private set; }
+
+        public Sha256(XElement xe, Workflow wf) : base(xe, wf)
         {
+            SmbComputerName = GetSetting("smbComputerName");
+            SmbDomain = GetSetting("smbDomain");
+            SmbUsername = GetSetting("smbUsername");
+            SmbPassword = GetSetting("smbPassword");
         }
 
         public override TaskStatus Run()
         {
             Info("Generating SHA-256 hashes...");
 
-            bool success = true;
-            bool atLeastOneSucceed = false;
+            var success = true;
+            var atLeastOneSuccess = false;
 
+            try
+            {
+                if (!string.IsNullOrEmpty(SmbComputerName) && !string.IsNullOrEmpty(SmbUsername) && !string.IsNullOrEmpty(SmbPassword))
+                {
+                    using (NetworkShareAccesser.Access(SmbComputerName, SmbDomain, SmbUsername, SmbPassword))
+                    {
+                        success = GenerateSha256(ref atLeastOneSuccess);
+                    }
+                }
+                else
+                {
+                    success = GenerateSha256(ref atLeastOneSuccess);
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                ErrorFormat("An error occured while generating SHA-256.", e);
+                success = false;
+            }
+
+            var status = Status.Success;
+
+            if (!success && atLeastOneSuccess)
+            {
+                status = Status.Warning;
+            }
+            else if (!success)
+            {
+                status = Status.Error;
+            }
+
+            Info("Task finished.");
+            return new TaskStatus(status, false);
+        }
+
+        private bool GenerateSha256(ref bool atLeastOneSuccess)
+        {
+            var success = true;
             var files = SelectFiles();
 
             if (files.Length > 0)
@@ -43,8 +93,8 @@ namespace Wexflow.Tasks.Sha256
                                 new XAttribute("sha256", sha1)));
                         }
                         InfoFormat("SHA-256 hash of the file {0} is {1}", file.Path, sha1);
-                        
-                        if (!atLeastOneSucceed) atLeastOneSucceed = true;
+
+                        if (!atLeastOneSuccess) atLeastOneSuccess = true;
                     }
                     catch (ThreadAbortException)
                     {
@@ -59,20 +109,7 @@ namespace Wexflow.Tasks.Sha256
                 xdoc.Save(md5Path);
                 Files.Add(new FileInf(md5Path, Id));
             }
-
-            var status = Status.Success;
-
-            if (!success && atLeastOneSucceed)
-            {
-                status = Status.Warning;
-            }
-            else if (!success)
-            {
-                status = Status.Error;
-            }
-
-            Info("Task finished.");
-            return new TaskStatus(status, false);
+            return success;
         }
 
         private string GetSha1(string filePath)
