d99f19f6ce8b516be2dd0cf91b46bcea6db3c091
aelassas <akram.elassas@gmail.com>
2020-04-27 12:34:26 +0000

    Added samba support to Tgz (#219)

--- src/dotnet/Wexflow.Tasks.Tgz/Tgz.cs
+++ src/dotnet/Wexflow.Tasks.Tgz/Tgz.cs
@@ -11,19 +11,64 @@ namespace Wexflow.Tasks.Tgz
     public class Tgz : Task
     {
         public string TgzFileName { get; private set; }
+        public string SmbComputerName { get; private set; }
+        public string SmbDomain { get; private set; }
+        public string SmbUsername { get; private set; }
+        public string SmbPassword { get; private set; }
 
-        public Tgz(XElement xe, Workflow wf)
-            : base(xe, wf)
+        public Tgz(XElement xe, Workflow wf) : base(xe, wf)
         {
             TgzFileName = GetSetting("tgzFileName");
+            SmbComputerName = GetSetting("smbComputerName");
+            SmbDomain = GetSetting("smbDomain");
+            SmbUsername = GetSetting("smbUsername");
+            SmbPassword = GetSetting("smbPassword");
         }
 
         public override TaskStatus Run()
         {
             Info("Creating tgz archive...");
 
-            bool success = true;
+            var success = true;
 
+            try
+            {
+                if (!string.IsNullOrEmpty(SmbComputerName) && !string.IsNullOrEmpty(SmbUsername) && !string.IsNullOrEmpty(SmbPassword))
+                {
+                    using (NetworkShareAccesser.Access(SmbComputerName, SmbDomain, SmbUsername, SmbPassword))
+                    {
+                        success = CreateTgz();
+                    }
+                }
+                else
+                {
+                    success = CreateTgz();
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                ErrorFormat("An error occured while creating tgz.", e);
+                success = false;
+            }
+
+            var status = Status.Success;
+
+            if (!success)
+            {
+                status = Status.Error;
+            }
+
+            Info("Task finished.");
+            return new TaskStatus(status, false);
+        }
+
+        private bool CreateTgz()
+        {
+            var success = true;
             var files = SelectFiles();
             if (files.Length > 0)
             {
@@ -88,16 +133,8 @@ namespace Wexflow.Tasks.Tgz
                     success = false;
                 }
             }
-
-            var status = Status.Success;
-
-            if (!success)
-            {
-                status = Status.Error;
-            }
-
-            Info("Task finished.");
-            return new TaskStatus(status, false);
+            return success;
         }
+
     }
 }
