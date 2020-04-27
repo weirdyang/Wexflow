15c6851a9986a2c9d6ddfce4c25f5e61ea09cd4a
aelassas <akram.elassas@gmail.com>
2020-04-27 17:32:26 +0000

    Added samba support to ImagesConcat (#219)

--- src/dotnet/Wexflow.Tasks.ImagesConcat/ImagesConcat.cs
+++ src/dotnet/Wexflow.Tasks.ImagesConcat/ImagesConcat.cs
@@ -10,16 +10,63 @@ namespace Wexflow.Tasks.ImagesConcat
 {
     public class ImagesConcat : Task
     {
-        public ImagesConcat(XElement xe, Workflow wf)
-          : base(xe, wf)
+        public string SmbComputerName { get; private set; }
+        public string SmbDomain { get; private set; }
+        public string SmbUsername { get; private set; }
+        public string SmbPassword { get; private set; }
+
+        public ImagesConcat(XElement xe, Workflow wf) : base(xe, wf)
         {
+            SmbComputerName = GetSetting("smbComputerName");
+            SmbDomain = GetSetting("smbDomain");
+            SmbUsername = GetSetting("smbUsername");
+            SmbPassword = GetSetting("smbPassword");
         }
 
         public override TaskStatus Run()
         {
             Info("Concatenating images...");
-            Status status = Status.Success;
 
+            var success = true;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(SmbComputerName) && !string.IsNullOrEmpty(SmbUsername) && !string.IsNullOrEmpty(SmbPassword))
+                {
+                    using (NetworkShareAccesser.Access(SmbComputerName, SmbDomain, SmbUsername, SmbPassword))
+                    {
+                        success = Concat();
+                    }
+                }
+                else
+                {
+                    success = Concat();
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                ErrorFormat("An error occured while copying files.", e);
+                success = false;
+            }
+
+            var status = Status.Success;
+
+            if (!success)
+            {
+                status = Status.Error;
+            }
+
+            Info("Task finished");
+            return new TaskStatus(status);
+        }
+
+        private bool Concat()
+        {
+            var success = true;
             try
             {
                 var imageFiles = SelectFiles();
@@ -31,11 +78,7 @@ namespace Wexflow.Tasks.ImagesConcat
                     var destPath = Path.Combine(Workflow.WorkflowTempFolder,
                             string.Format("ImagesConcat_{0:yyyy-MM-dd-HH-mm-ss-fff}{1}", DateTime.Now, extension));
 
-                    var res = ConcatImages(imageFiles, destPath);
-                    if (!res)
-                    {
-                        status = Status.Error;
-                    }
+                    success = ConcatImages(imageFiles, destPath);
                 }
                 else if (imageFiles.Length == 1)
                 {
@@ -49,13 +92,10 @@ namespace Wexflow.Tasks.ImagesConcat
             catch (Exception e)
             {
                 ErrorFormat("An error occured while concatenating images: {0}", e.Message);
-                status = Status.Error;
+                success = false;
             }
-
-            Info("Task finished");
-            return new TaskStatus(status);
+            return success;
         }
-
 
         private bool ConcatImages(FileInf[] imageFiles, string destPath)
         {
