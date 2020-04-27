926ed64bc9ba6fc6763dbc2f47e329599e6390b9
aelassas <akram.elassas@gmail.com>
2020-04-27 12:22:07 +0000

    Added samba support to Tar (#219)

--- src/dotnet/Wexflow.Tasks.Tar/Tar.cs
+++ src/dotnet/Wexflow.Tasks.Tar/Tar.cs
@@ -10,19 +10,64 @@ namespace Wexflow.Tasks.Tar
     public class Tar : Task
     {
         public string TarFileName { get; private set; }
+        public string SmbComputerName { get; private set; }
+        public string SmbDomain { get; private set; }
+        public string SmbUsername { get; private set; }
+        public string SmbPassword { get; private set; }
 
-        public Tar(XElement xe, Workflow wf)
-            : base(xe, wf)
+        public Tar(XElement xe, Workflow wf) : base(xe, wf)
         {
             TarFileName = GetSetting("tarFileName");
+            SmbComputerName = GetSetting("smbComputerName");
+            SmbDomain = GetSetting("smbDomain");
+            SmbUsername = GetSetting("smbUsername");
+            SmbPassword = GetSetting("smbPassword");
         }
 
         public override TaskStatus Run()
         {
             Info("Creating tar archive...");
 
-            bool success = true;
+            var success = true;
 
+            try
+            {
+                if (!string.IsNullOrEmpty(SmbComputerName) && !string.IsNullOrEmpty(SmbUsername) && !string.IsNullOrEmpty(SmbPassword))
+                {
+                    using (NetworkShareAccesser.Access(SmbComputerName, SmbDomain, SmbUsername, SmbPassword))
+                    {
+                        success = CreateTar();
+                    }
+                }
+                else
+                {
+                    success = CreateTar();
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                ErrorFormat("An error occured while creating tar.", e);
+                success = false;
+            }
+
+            var status = Status.Success;
+
+            if (!success)
+            {
+                status = Status.Error;
+            }
+
+            Info("Task finished.");
+            return new TaskStatus(status, false);
+        }
+
+        private bool CreateTar()
+        {
+            var success = true;
             var files = SelectFiles();
             if (files.Length > 0)
             {
@@ -86,16 +131,8 @@ namespace Wexflow.Tasks.Tar
                     success = false;
                 }
             }
-
-            var status = Status.Success;
-
-            if (!success)
-            {
-                status = Status.Error;
-            }
-
-            Info("Task finished.");
-            return new TaskStatus(status, false);
+            return success;
         }
+
     }
 }
