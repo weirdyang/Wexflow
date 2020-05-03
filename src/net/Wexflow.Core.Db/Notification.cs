e89b47f2e75a1280aadfe8c9ec33e70adc5478b3
aelassas <akram.elassas@gmail.com>
2020-05-03 21:32:43 +0000

    Added Notification.cs

---
+++ src/net/Wexflow.Core.Db/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Wexflow.Core.Db
+{
+    public class Notification
+    {
+        public static readonly string DocumentName = "notifications";
+
+        public string AssignedBy { get; set; }
+        public DateTime AssignedOn { get; set; }
+        public string Message { get; set; }
+
+        public virtual string GetDbId()
+        {
+            return "-1";
+        }
+    }
+}
