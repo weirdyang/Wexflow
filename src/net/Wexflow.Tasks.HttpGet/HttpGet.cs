e6ef23473777c29224cc019288dda06d958e19ce
aelassas <akram.elassas@gmail.com>
2020-05-03 16:55:01 +0000

    Updated HttpGet task (#230)

--- src/net/Wexflow.Tasks.HttpGet/HttpGet.cs
+++ src/net/Wexflow.Tasks.HttpGet/HttpGet.cs
@@ -1,23 +1,29 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Authentication;
+using System.Text;
 using System.Threading;
 using System.Xml.Linq;
 using Wexflow.Core;
 
 namespace Wexflow.Tasks.HttpGet
 {
-    public class HttpGet:Task
+    public class HttpGet : Task
     {
-        private const SslProtocols _Tls12 = (SslProtocols)0x00000C00;
-        private const SecurityProtocolType Tls12 = (SecurityProtocolType)_Tls12;
-
         public string Url { get; private set; }
+        public string Payload { get; private set; }
+        public string Authorization { get; private set; }
+        public string Bearer { get; private set; }
 
         public HttpGet(XElement xe, Workflow wf) : base(xe, wf)
         {
             Url = GetSetting("url");
+            Payload = GetSetting("payload");
+            Authorization = GetSetting("authorization");
+            Bearer = GetSetting("bearer");
         }
 
         public override TaskStatus Run()
@@ -26,17 +32,13 @@ namespace Wexflow.Tasks.HttpGet
             var status = Status.Success;
             try
             {
-                using (var client = new WebClient())
-                {
-                    ServicePointManager.Expect100Continue = true;
-                    ServicePointManager.SecurityProtocol = Tls12;
-
-                    var response = client.DownloadData(Url);
-                    var destFile = Path.Combine(Workflow.WorkflowTempFolder, string.Format("HttpGet_{0:yyyy-MM-dd-HH-mm-ss-fff}", DateTime.Now));
-                    File.WriteAllBytes(destFile, response);
-                    Files.Add(new FileInf(destFile, Id));
-                    InfoFormat("GET request {0} executed whith success -> {1}", Url, destFile);
-                }
+                var getTask = Post(Url, Authorization, Bearer, Payload);
+                getTask.Wait();
+                var result = getTask.Result;
+                var destFile = Path.Combine(Workflow.WorkflowTempFolder, string.Format("HttpGet_{0:yyyy-MM-dd-HH-mm-ss-fff}", DateTime.Now));
+                File.WriteAllText(destFile, result);
+                Files.Add(new FileInf(destFile, Id));
+                InfoFormat("GET request {0} executed whith success -> {1}", Url, destFile);
             }
             catch (ThreadAbortException)
             {
@@ -49,6 +51,28 @@ namespace Wexflow.Tasks.HttpGet
             }
             Info("Task finished.");
             return new TaskStatus(status);
+        }
+
+        public async System.Threading.Tasks.Task<string> Post(string url, string auth, string bearer, string payload)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                if (!string.IsNullOrEmpty(auth))
+                {
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", auth);
+                }
+                else if (!string.IsNullOrEmpty(bearer))
+                {
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearer);
+                }
+                var httpResponse = await httpClient.GetAsync(url);
+                if (httpResponse.Content != null)
+                {
+                    var responseContent = await httpResponse.Content.ReadAsStringAsync();
+                    return responseContent;
+                }
+            }
+            return string.Empty;
         }
     }
 }
